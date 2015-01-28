008758de2017964c0db0f777b69f9cf1a400d6b1
Spencer Williams <s@spencerenglish.com>
2015-01-28 11:37:30 -0800

    string format implementation

---
+++ NSString+FormatHelpers.h
@@ -0,0 +1,15 @@
+//
+//  NSString+FormatHelpers.h
+//  CurrentScience
+//
+//  Created by Spencer Williams on 1/28/15.
+//  This is free and unencumbered software released into the public domain.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString(FormatHelpers)
+- (NSString *)stringByStrippingLinks;
+/// Purely a helper method to call GTMNSString+HTML's `-gtm_stringByEscapingFromHTML`
+- (NSString *)stringByDecodingHTMLEntities;
+@end
